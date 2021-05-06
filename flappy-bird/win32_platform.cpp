8a1fa4c4d409d304c114aaca7270573abd70c2d7
Rylan Chin <rchin@ualberta.ca>
2021-05-05 23:18:27 -0600

    setup window buffer

--- flappy-bird/win32_platform.cpp
+++ flappy-bird/win32_platform.cpp
@@ -1,7 +1,44 @@
 #include <Windows.h>
 
+bool running = true;
+
+void* buffer;
+int buffer_width;
+int buffer_height;
+BITMAPINFO buffer_bitmap_info;
+
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	LRESULT result = 0;
+	switch (uMsg) {
+		case WM_CLOSE:
+		case WM_DESTROY: {
+			running = false;
+		} break;
+		case WM_SIZE: {
+			RECT rect;
+			GetClientRect(hwnd, &rect);
+			buffer_width = rect.right - rect.left;
+			buffer_height = rect.bottom - rect.top;
+
+			int buffer_size = buffer_width * buffer_height * sizeof(unsigned int);
+
+			if (buffer)
+				VirtualFree(buffer, 0, MEM_RELEASE);
+			buffer = VirtualAlloc(0, buffer_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+			buffer_bitmap_info.bmiHeader.biSize = sizeof(buffer_bitmap_info.bmiHeader);
+			buffer_bitmap_info.bmiHeader.biWidth = buffer_width;
+			buffer_bitmap_info.bmiHeader.biHeight = buffer_height;
+			buffer_bitmap_info.bmiHeader.biPlanes = 1;
+			buffer_bitmap_info.bmiHeader.biBitCount = 32;
+			buffer_bitmap_info.bmiHeader.biCompression = BI_RGB;
+
+		} break;
+		default: {
+			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}
+	}
+	return result;
 }
 
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
@@ -18,17 +55,28 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 	RegisterClass(&wc);
 
 	// Create window
-	HWND hwnd = CreateWindowEx(0, CLASS_NAME, L"I tried making Flappy Bird in C++", WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+	HWND hwnd = CreateWindowEx(0, CLASS_NAME, L"I tried making Flappy Bird in C++", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+		CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720,
 		NULL, NULL,
 		hInstance,
 		NULL);
+
+	HDC hdc = GetDC(hwnd);
 
 	if (hwnd == NULL)
 		return 0;
 
 	ShowWindow(hwnd, nShowCmd);
 
+	while (running) {
+		MSG message;
+		while (PeekMessage(&message, hwnd, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&message);
+			DispatchMessage(&message);
+		}
+
+		StretchDIBits(hdc, 0, 0, buffer_width, buffer_height, 0, 0, buffer_width, buffer_height, buffer, &buffer_bitmap_info, DIB_RGB_COLORS, SRCCOPY);
+	}
 
 	return 0;
 }
